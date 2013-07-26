99105289aebba9385e34af9994c5a15a2a5da5e6
kensakukomatsu <kensaku.komatsu@gmail.com>
2013-07-26 16:28:22 +0900

    remove option from app.js

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(options, app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });

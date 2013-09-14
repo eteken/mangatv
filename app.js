180759de9d21a22d4cbbe0c1908954cd9d3cfa36
Toru Yoshikawa <pikotea@gmail.com>
2013-09-15 01:36:57 +0900

    bbb

--- app.js
+++ app.js
@@ -34,3 +34,79 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+app.post('/upload/movies/:fileName', function(req, res) {
+    var actorTwitterId = req.body.actorTwitterId;
+    var actorName = req.body.actorName;
+    console.log(req.body);
+    var movieId = req.body.movieId;
+    var movieFile = req.files.movie;
+    var fileName = movieFile.filename;
+    var uploadedFilePath = movieFile.path;
+    var targetPath = '/upload/movies/' + fileName;
+    var saveFilePath = getUploadedMoviePath(fileName);
+    fs.rename(uploadedFilePath, saveFilePath, function (err) {
+        if (err) {
+            res.send(500, err);
+        } else {
+            movieData.push({
+                id: movieId,
+                fileName: fileName,
+                path: targetPath,
+                actor: {
+                    twitterId: actorTwitterId,
+                    name: actorName
+                }
+            });
+            storeMovieData(function(err) {
+                if (err) {
+                    console.error(err);
+                }
+            });
+            res.send(200, targetPath);
+        }
+    });
+});
+
+app.get('/movies', function(req, res) {
+    res.render('movies/index', { movieData: movieData });
+});
+
+app.get('/movies/:id', function(req, res) {
+    var movieId = req.params.id;
+    var targetIdx = -1;
+    var max = movieData.length;
+    for (var i = 0; i < max; i++) {
+        if (movieData[i].id == movieId) {
+            targetIdx = i;
+            break;
+        }
+    }
+    if (targetIdx === -1) {
+        return res.send(404);
+    }
+    var prev = null, next = null;
+    var target = movieData[targetIdx];
+    if (targetIdx > 0) {
+        prev = movieData[targetIdx - 1];
+    }
+    if (targetIdx < max - 1) {
+        next = movieData[targetIdx + 1];
+    }
+    res.render('movies/detail', {
+        target: target,
+        prev: prev,
+        next: next
+    });
+});
+
+function getUploadedMoviePath(fileName) {
+    var targetPath = '/upload/movies/' + fileName;
+    var saveFilePath = __dirname + '/public' + targetPath;
+    return saveFilePath;
+}
+function storeMovieData(callback) {
+    fs.writeFile(movieDataFilePath, JSON.stringify(movieData), function(err) {
+        callback(err);
+    });
+}

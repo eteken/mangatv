9c951594a88f91a105cf94dfbedd3ca438ca4286
Toshiya Nakakrua <t.nakakura@ntt.com>
2013-10-22 21:08:52 +0900

    undo機能追加 + beginPath()追加

--- public/js/Deco.js
+++ public/js/Deco.js
@@ -1,6 +1,31 @@
 var Deco;
 
 (function(global){
+    UndoManager = function(){
+        this.cPushArray = [];
+        this.cStep = -1;
+    }
+    UndoManager.prototype = {
+        push: function(dataURL){
+            this.cStep++;
+            if (this.cStep < this.cPushArray.length) { this.cPushArray.length = this.cStep; }
+            this.cPushArray.push(dataURL);
+        },
+        undo: function(ctx){
+            console.log("undo" + this.cStep);
+            if (this.cStep > 0) {
+                this.cStep--;
+                var canvasPic = new Image();
+                canvasPic.src = this.cPushArray[this.cStep];
+                console.log(canvasPic.src);
+                canvasPic.addEventListener("load", function () {
+                    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+                    ctx.drawImage(canvasPic, 0, 0);
+                }.bind(ctx, canvasPic));
+            }
+        }
+    }
+
 	Deco = function(options){
 		new EventEmitter().apply(this);
 
@@ -31,11 +56,13 @@ var Deco;
 	}
 
 	Deco.prototype.handler = function(){
+        console.log("handler");
 		var drawing = false, prev = null;
 		this.ctx = this.canvas.getContext('2d');
 		this.ctx.lineWidth = this.opts.line_width;
 		this.ctx.strokeStyle = this.opts.line_color;
 		this.ctx.lineCap = "round";
+        this.undoManger = new UndoManager();
 
 		this.opts.node.addEventListener("mousedown", function(){
 			drawing = true;
@@ -44,11 +71,13 @@ var Deco;
 		this.opts.node.addEventListener("mouseup", function(){
 			drawing = false;
 			prev = null;
-		}, false)
+            this.undoManger.push(this.canvas.toDataURL());
+		}.bind(this), false)
 		this.opts.node.addEventListener("mousemove", function(e){
 			if(drawing){
 				var x = e.layerX, y = e.layerY;
 				if(!!prev) {
+                    this.ctx.beginPath();
 					this.ctx.moveTo(prev.x, prev.y);
 					this.ctx.lineTo(x, y);
 					this.ctx.stroke();
@@ -70,6 +99,7 @@ var Deco;
                 var x = touches[0].pageX - this.canvasRect.left;
                 var y = touches[0].pageY - this.canvasRect.top;
                 if(!!prev) {
+                    this.ctx.beginPath();
                     this.ctx.moveTo(prev.x, prev.y);
                     this.ctx.lineTo(x, y);
                     this.ctx.stroke();
@@ -83,17 +113,25 @@ var Deco;
             e.preventDefault();
             drawing = false;
             prev = null;
-        }, false)
+            this.undoManger.push(this.canvas.toDataURL());
+        }.bind(this), false)
         this.opts.node.addEventListener("touchcancel", function(e){
             e.preventDefault();
             drawing = false;
             prev = null;
-        }, false)
+            this.undoManger.push(this.canvas.toDataURL());
+        }.bind(this), false)
         this.opts.node.addEventListener("touchleave", function(e){
             e.preventDefault();
             drawing = false;
             prev = null;
-        }, false)
+            this.undoManger.push(this.canvas.toDataURL());
+        }.bind(this), false)
+
+        var self = this;
+        $('#undo').on('click', function(){
+            this.undoManger.undo(this.ctx);
+        }.bind(this))
 	}
 
 	Deco.prototype.getContext = function(){
